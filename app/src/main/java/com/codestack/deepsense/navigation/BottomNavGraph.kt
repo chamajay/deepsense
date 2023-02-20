98d7a16011a51795eda5e3e2fd2a6b06a7f89e9d
Chamath Jayasena <chamath.jayasena@protonmail.com>
2023-02-20 14:23:23 +0530

    Add bottom navigation logic

---
+++ app/src/main/java/com/codestack/deepsense/navigation/BottomNavGraph.kt
@@ -0,0 +1,39 @@
+package com.codestack.deepsense.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.codestack.deepsense.screens.EmptyScreen
+import com.codestack.deepsense.screens.activity.ActivityScreen
+import com.codestack.deepsense.screens.home.HomeScreen
+
+@Composable
+fun BottomNavGraph(
+    navController: NavHostController,
+    bottomNavController: NavHostController,
+    paddingValues: PaddingValues
+) {
+    NavHost(
+        navController = bottomNavController,
+        startDestination = BottomBarScreen.Home.route
+    ) {
+        composable(
+            route = BottomBarScreen.Home.route
+        ) {
+            HomeScreen(navController = navController, paddingValues = paddingValues)
+        }
+        composable(
+            route = BottomBarScreen.Activity.route
+        ) {
+            ActivityScreen()
+        }
+        composable(
+            route = BottomBarScreen.Suggestions.route
+        ) {
+            EmptyScreen()
+        }
+    }
+
+}
