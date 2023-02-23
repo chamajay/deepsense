7f699d088c5fb8954001a0b5d902795ad448df10
Chamath Jayasena <chamath.jayasena@protonmail.com>
2023-02-23 15:12:32 +0530

    Improve main top bar

--- app/src/main/java/com/codestack/deepsense/components/MainTopAppBar.kt
+++ app/src/main/java/com/codestack/deepsense/components/MainTopAppBar.kt
@@ -1,9 +1,7 @@
 package com.codestack.deepsense.components
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AccountCircle
 import androidx.compose.material3.Icon
@@ -11,10 +9,13 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.codestack.deepsense.navigation.Screens
@@ -24,43 +25,32 @@ import com.codestack.deepsense.ui.theme.DeepSenseTheme
 fun MainTopAppBar(
     navController: NavHostController
 ) {
-    val user = "Chamath"
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(18.dp, 10.dp)
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-            .padding(6.dp)
-            .weight(1f)
-        ) {
+    TopAppBar(
+        title = {
             Text(
-                text = "Hello ",
-                fontSize = MaterialTheme.typography.headlineSmall.fontSize
+                buildAnnotatedString {
+                    withStyle(style = SpanStyle()) {
+                        append("Hello ")
+                    }
+                    withStyle(style = SpanStyle(color = MaterialTheme.colorScheme.primary)) {
+                        append("Chamath")
+                    }
+                    append("!")
+                }, fontSize = 20.sp
             )
-            Text(
-                text = user,
-                fontSize = MaterialTheme.typography.headlineSmall.fontSize,
-                color = MaterialTheme.colorScheme.primary
-            )
-            Text(
-                text = "!",
-                fontSize = MaterialTheme.typography.headlineSmall.fontSize
-            )
-        }
-        IconButton(
-            onClick = { navController.navigate(Screens.Settings.route) },
-        ) {
-            Icon(
-                Icons.Outlined.AccountCircle,
-                contentDescription = "Profile Button",
-                modifier = Modifier.size(32.dp)
-            )
-        }
-    }
+        },
+        actions = {
+            IconButton(onClick = { navController.navigate(Screens.Settings.route) }) {
+                Icon(
+                    Icons.Outlined.AccountCircle,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(30.dp)
+                )
+            }
+        },
+        backgroundColor = MaterialTheme.colorScheme.background
+    )
+
 }
 
 @Preview(showBackground = true)
