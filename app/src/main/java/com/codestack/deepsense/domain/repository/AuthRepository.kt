e8bbe4ca88895295d1e2269c01ebf0486686b8f6
Chamath Jayasena <chamath.jayasena@protonmail.com>
2023-02-24 12:36:20 +0530

    Add auth repo interface and implementation

---
+++ app/src/main/java/com/codestack/deepsense/domain/repository/AuthRepository.kt
@@ -0,0 +1,16 @@
+package com.codestack.deepsense.domain.repository
+
+import com.codestack.deepsense.domain.model.Response
+import com.google.android.gms.auth.api.identity.BeginSignInResult
+import com.google.firebase.auth.AuthCredential
+
+typealias OneTapSignInResponse = Response<BeginSignInResult>
+typealias SignInWithGoogleResponse = Response<Boolean>
+
+interface AuthRepository {
+    val isUserAuthenticatedInFirebase: Boolean
+
+    suspend fun oneTapSignInWithGoogle(): OneTapSignInResponse
+
+    suspend fun firebaseSignInWithGoogle(googleCredential: AuthCredential): SignInWithGoogleResponse
+}
