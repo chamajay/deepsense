828a43f450b8d03f9f21dc98a6fda5a6b58eba2e
TT <95086384+cameliaclover@users.noreply.github.com>
2023-03-01 17:26:13 +0530

    improve About us screen

--- app/src/main/java/com/codestack/deepsense/presentation/aboutus/AboutUsScreen.kt
+++ app/src/main/java/com/codestack/deepsense/presentation/aboutus/AboutUsScreen.kt
@@ -9,21 +9,18 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.codestack.deepsense.R
 
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AboutUsScreen() {
 
-    Surface{
-        Column(modifier=Modifier.fillMaxHeight()) {
-
+    Surface {
+        Column(modifier = Modifier.fillMaxHeight())
+        {
 
             Row {
                 Icon(
@@ -40,7 +37,13 @@ fun AboutUsScreen() {
                 )
                 Spacer(modifier = Modifier.height(10.dp))
             }
-            Row {
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
                 Spacer(modifier = Modifier.width(10.dp))
                 Text(
                     text = "We are a team of passionate developers who are Software engineering undergraduate students of informatics institute of technology. We are dedicated to delivering high-quality apps to make people's lives easier.",
@@ -49,118 +52,74 @@ fun AboutUsScreen() {
                 )
 
             }
-            Spacer(modifier = Modifier.height(5.dp))
-                Spacer(modifier = Modifier.height(10.dp))
-                Row {
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Text("Team Members:",fontWeight = FontWeight.Bold)
+            Spacer(modifier = Modifier.height(15.dp))
+            Row {
+                Spacer(modifier = Modifier.width(10.dp))
+                Text("Team Members:", fontWeight = FontWeight.Bold)
 
-                }
-                Spacer(modifier = Modifier.height(10.dp))
+            }
+            Spacer(modifier = Modifier.height(15.dp))
+            MemberInfo(
+                "member1",
+                memImage = R.drawable.account_circle_48px,
+                name = " Chamath Jayasena "
+            )
+            MemberInfo(
+                "member2",
+                memImage = R.drawable.account_circle_48px,
+                name = " Kumudu Wijewardhana "
+            )
+            MemberInfo(
+                "member3",
+                memImage = R.drawable.account_circle_48px,
+                name = " Thimasha Thakshali  "
+            )
+            MemberInfo(
+                "member4",
+                memImage = R.drawable.account_circle_48px,
+                name = " Ruwindi Perera "
+            )
+            MemberInfo(
+                "member5",
+                memImage = R.drawable.account_circle_48px,
+                name = " Sahan Dissanayake "
+            )
 
-                Card(Modifier.size(width = 380.dp, height = 90.dp)) {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Spacer(modifier = Modifier.width(10.dp))
-                        Icon(
-                            painter = painterResource(R.drawable.account_circle_48px),
-                            "member1",
-                            modifier = Modifier
-                                .size(80.dp)
-                                .clip(
-                                    RoundedCornerShape(50.dp)
-                                )
-                        )
-                        Text(
-                            text = " Chamath Jayasena ",
-                        )
-                    }}
-
-            Spacer(modifier = Modifier.height(10.dp))
-
-            Card(Modifier.size(width = 380.dp, height = 90.dp)) {
-
-                Row(verticalAlignment = Alignment.CenterVertically) {
-
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Icon(
-                        painter = painterResource(R.drawable.account_circle_48px),
-                        "member2",
-                        modifier = Modifier
-                            .size(80.dp)
-                            .clip(
-                                RoundedCornerShape(50.dp)
-                            )
-                    )
-                    Text(
-                        text = " Kumudu Wijewardhana ",
-                    )
-                } }
-
-            Spacer(modifier = Modifier.height(10.dp))
-
-            Card(Modifier.size(width = 380.dp, height = 90.dp)) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Icon(
-                        painter = painterResource(R.drawable.account_circle_48px),
-                        "member3",
-                        modifier = Modifier
-                            .size(80.dp)
-                            .clip(
-                                RoundedCornerShape(50.dp)
-                            )
-                    )
-                    Text(
-                        text = " Thimasha Thakshali ",
-                        textAlign = TextAlign.Right
-                    )
-                }}
-
-            Spacer(modifier = Modifier.height(10.dp))
-
-            Card(Modifier.size(width = 380.dp, height = 90.dp)) {
-
-                Row(verticalAlignment = Alignment.CenterVertically) {
-
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Icon(
-                        painter = painterResource(R.drawable.account_circle_48px),
-                        "member4",
-                        modifier = Modifier
-                            .size(80.dp)
-                            .clip(
-                                RoundedCornerShape(50.dp)
-                            )
-                    )
-                    Text(
-                        text = " Ruwindi Perera ",
-                        textAlign = TextAlign.Right
-                    )
-                }}
-
-            Spacer(modifier = Modifier.height(10.dp))
-
-            Card(Modifier.size(width = 380.dp, height =90.dp)) {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Icon(
-                        painter = painterResource(R.drawable.account_circle_48px),
-                        "member5",
-                        modifier = Modifier
-                            .size(80.dp)
-                            .clip(
-                                RoundedCornerShape(50.dp)
-                            )
-                    )
-                    Text(
-                        text = " Sahan Dissanayake ",
-                        textAlign = TextAlign.Right
-                    )
-                }}
         }
     }
+}
 
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MemberInfo(
+    memNo : String,
+    memImage: Int,
+    name: String
+
+) {
+    Spacer(modifier = Modifier.height(10.dp))
+    Row(modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ){
+        Card(Modifier.size(width = 350.dp, height = 80.dp)) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Icon(
+                    painter = painterResource(id = memImage),
+                    memNo,
+                    modifier = Modifier
+                        .size(80.dp)
+                        .clip(
+                            RoundedCornerShape(50.dp)
+                        )
+                )
+                Text(
+                    text = name,
+                )
+            }
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+    }
 }
 
 @Preview
