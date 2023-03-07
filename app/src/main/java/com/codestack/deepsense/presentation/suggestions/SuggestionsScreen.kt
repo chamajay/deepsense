d743ef2c7f139119a3592364c8a509b4f1f5452e
sahandissanayake <sahandissanayake419@gmail.com>
2023-03-07 11:39:32 +0530

    optimize imports

--- app/src/main/java/com/codestack/deepsense/presentation/suggestions/SuggestionsScreen.kt
+++ app/src/main/java/com/codestack/deepsense/presentation/suggestions/SuggestionsScreen.kt
@@ -3,7 +3,6 @@ package com.codestack.deepsense.presentation.suggestions
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
