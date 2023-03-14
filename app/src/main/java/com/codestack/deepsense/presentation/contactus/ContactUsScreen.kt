e8cf456f87fc47f9769b7e96311dcec89001c53b
TT <95086384+cameliaclover@users.noreply.github.com>
2023-03-14 13:18:24 +0530

    Add CenterAlignedTopAppBar to the contact us screen

--- app/src/main/java/com/codestack/deepsense/presentation/contactus/ContactUsScreen.kt
+++ app/src/main/java/com/codestack/deepsense/presentation/contactus/ContactUsScreen.kt
@@ -1,7 +1,11 @@
 package com.codestack.deepsense.presentation.contactus
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,10 +18,36 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.codestack.deepsense.R
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ContactUsScreen()
 {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Contact Us",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 27.sp
+                    )
+
+                },
+                navigationIcon = { Icon(Icons.Filled.ArrowBack, contentDescription = "Go Back") },
+                //actions = { /* create your action items here */ }
+                )
+        },
+        content = {
+
+            Content()
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Content() {
     Surface {
         Column(
             modifier = Modifier
@@ -26,118 +56,119 @@ fun ContactUsScreen()
                 .fillMaxHeight()
         )
         {
-            Row {
-                Icon(
-                    painter = painterResource(R.drawable.arrow_back_48px),
-                    "back arrow",
-                    modifier = Modifier
-                        .size(40.dp)
-                )
-                Spacer(modifier = Modifier.width(60.dp))
+            Spacer(modifier = Modifier.height(80.dp))
+            LazyColumn() {
 
-                Text(
-                    text = "Contact Us",
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 30.sp
-                )
-                Spacer(modifier = Modifier.height(70.dp))
-            }
+                item {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
 
+                        Card(Modifier.size(width = 350.dp, height = 140.dp)) {
+                            Spacer(modifier = Modifier.height(20.dp))
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Spacer(modifier = Modifier.width(30.dp))
+                                Icon(
+                                    painter = painterResource(R.drawable.mail_48px),
+                                    "mail",
+                                    modifier = Modifier
+                                        .size(40.dp)
+                                        .clip(
+                                            RoundedCornerShape(50.dp)
+                                        )
 
-                Card(Modifier.size(width = 350.dp, height = 140.dp)) {
-                    Spacer(modifier = Modifier.height(20.dp))
-
-                    Row(modifier = Modifier.fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically) {
-                        Spacer(modifier = Modifier.width(30.dp))
-                        Icon(
-                            painter = painterResource(R.drawable.mail_48px),
-                            "mail",
-                            modifier = Modifier
-                                .size(40.dp)
-                                .clip(
-                                    RoundedCornerShape(50.dp)
                                 )
+                                Text(text = "  XXXXX.XXXXXXXX@iit.ac.lk")
 
-                        )
-                        Text(text = "  XXXXX.XXXXXXXX@iit.ac.lk")
+                            }
+                            Spacer(modifier = Modifier.height(20.dp))
 
-                    }
-                    Spacer(modifier = Modifier.height(20.dp))
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Spacer(modifier = Modifier.width(30.dp))
+                                Icon(
+                                    painter = painterResource(R.drawable.call_48px),
+                                    "call",
+                                    modifier = Modifier
+                                        .size(40.dp)
+                                        .clip(
+                                            RoundedCornerShape(50.dp)
+                                        )
 
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Spacer(modifier = Modifier.width(30.dp))
-                        Icon(
-                            painter = painterResource(R.drawable.call_48px),
-                            "call",
-                            modifier = Modifier
-                                .size(40.dp)
-                                .clip(
-                                    RoundedCornerShape(50.dp)
                                 )
+                                Text(text = "  0XX-XXXXXXX")
 
-                        )
-                        Text(text = "  0XX-XXXXXXX")
+                            }
+                        }
 
                     }
                 }
-            }
+                item {
+                    Spacer(modifier = Modifier.height(50.dp))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Card(Modifier.size(width = 350.dp, height = 410.dp)) {
+                            Spacer(modifier = Modifier.height(20.dp))
 
-            Spacer(modifier = Modifier.height(40.dp))
+                            Text(
+                                text = "   Please fill out the form below to contact us.",
+                            )
+                            Spacer(modifier = Modifier.height(30.dp))
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center
+                            )
+                            {
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Card(Modifier.size(width = 350.dp, height = 410.dp)) {
-                    Spacer(modifier = Modifier.height(20.dp))
+                                OutlinedTextField(
+                                    value = "Name",
+                                    onValueChange = {},
+                                    label = { Text(text = "Name") }
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(25.dp))
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
 
-                    Text(
-                        text = "   Please fill out the form below to contact us.",
-                    )
-                    Spacer(modifier = Modifier.height(30.dp))
-                    Row(modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center)
-                    {
+                                OutlinedTextField(
+                                    value = "Email",
+                                    onValueChange = {},
+                                    label = { Text(text = "Email") }
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(25.dp))
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Center
+                            ) {
 
-                        OutlinedTextField(
-                            value = "Name",
-                            onValueChange = {},
-                            label = { Text(text = "Name") }
-                        )}
-                    Spacer(modifier = Modifier.height(25.dp))
-                    Row(modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center) {
+                                OutlinedTextField(
+                                    value = "message",
+                                    onValueChange = {},
+                                    label = { Text(text = "Message") },
+                                    maxLines = 10
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(25.dp))
+                            Row(modifier = Modifier.fillMaxWidth()) {
+                                Spacer(modifier = Modifier.width(200.dp))
+                                Button(onClick = { }) {
+                                    Text("Send Email")
+                                }
+                            }
+                        }
+                    }
 
-                        OutlinedTextField(
-                            value = "Email",
-                            onValueChange = {},
-                            label = { Text(text = "Email") }
-                    )}
-                    Spacer(modifier = Modifier.height(25.dp))
-                    Row(modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Center) {
-
-                        OutlinedTextField(
-                            value = "message",
-                            onValueChange = {},
-                            label = { Text(text = "Message") },
-                            maxLines = 10
-                    )}
-                    Spacer(modifier = Modifier.height(25.dp))
-                    Row(modifier = Modifier.fillMaxWidth()) {
-                        Spacer(modifier = Modifier.width(200.dp))
-                        Button(onClick = { }) {
-                            Text("Send Email")
-                    }}
                 }
             }
-
         }
     }
 }
