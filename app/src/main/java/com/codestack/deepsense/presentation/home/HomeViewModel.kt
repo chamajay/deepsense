39d7392a93bcb655ae1cb634b701bab015bf9114
Chamath Jayasena <chamath.jayasena@protonmail.com>
2023-03-25 20:25:49 +0530

    Implement backend connection to retrieve mood percentages

--- app/src/main/java/com/codestack/deepsense/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/codestack/deepsense/presentation/home/HomeViewModel.kt
@@ -1,6 +1,7 @@
 package com.codestack.deepsense.presentation.home
 
 import androidx.lifecycle.ViewModel
+import com.codestack.deepsense.R
 import com.codestack.deepsense.core.Constants.BASE_URL
 import com.codestack.deepsense.core.Utils
 import kotlinx.coroutines.*
@@ -8,6 +9,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import org.json.JSONArray
 import org.json.JSONObject
 
 class HomeViewModel : ViewModel() {
@@ -16,12 +18,17 @@ class HomeViewModel : ViewModel() {
     private val scope = CoroutineScope(Dispatchers.IO + job)
 
     private val _mood: MutableStateFlow<String> = MutableStateFlow("")
-    private val _moodImage: MutableStateFlow<Int> =
-        MutableStateFlow(0) // 0 is a default image resource ID
+    private val _moodImage: MutableStateFlow<Int> = MutableStateFlow(R.drawable.ic_home_outlined)
+    private val _emotionsPercentages: MutableStateFlow<MutableMap<String, Double>> = MutableStateFlow(
+        mutableMapOf()
+    )
+
     val mood: StateFlow<String>
         get() = _mood
     val moodImage: StateFlow<Int>
         get() = _moodImage
+    val emotionsPercentages: StateFlow<MutableMap<String, Double>>
+        get() = _emotionsPercentages
 
     fun retrieveMood() {
         scope.launch {
@@ -46,6 +53,37 @@ class HomeViewModel : ViewModel() {
                 val (mappedEmotion, imageResId) = Utils.mapEmotion(emotionFromServer)
                 _mood.value = mappedEmotion
                 _moodImage.value = imageResId
+            }
+        }
+    }
+
+    fun retrieveMoodPercentages() {
+        scope.launch {
+            // Reset before fetching
+            _emotionsPercentages.value = mutableMapOf()
+
+            val request = Request.Builder()
+                .url("$BASE_URL/today_mood_percentages")
+                .build()
+
+            // Execute request
+            val response = client.newCall(request).execute()
+
+            // Simulate a delay for loading animation
+            delay(700)
+
+            val responseBodyString = response.body?.string()
+            responseBodyString?.let {
+                val jsonObj = JSONObject(responseBodyString)
+                val jsonArr = JSONArray(jsonObj.getString("main_mood_percentages"))
+                for (i in 0 until jsonArr.length()) {
+                    val jsonObject = jsonArr.getJSONObject(i)
+                    val emotion = jsonObject.getString("emotion")
+                    val percentage = jsonObject.getDouble("percentage")
+                    _emotionsPercentages.value = _emotionsPercentages.value.toMutableMap().apply {
+                        put(emotion, percentage)
+                    }
+                }
             }
         }
     }
